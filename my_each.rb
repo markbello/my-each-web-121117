1235b2c3ca6ebbbfbeed47572527d7f70c98aa75
Mark Bello <mcbello51286@gmail.com>
2017-11-29 22:26:10 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  i = 0
+  while i < array.length
+    yield(arr[i]){|item|
+      puts item
+    }
+    i += 1
+  end
+  arr
 end
